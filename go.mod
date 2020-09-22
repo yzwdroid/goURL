a806213c1eb15d0b8ffdef8b423b16a166eb9b24
yzwdroid <yzwdroid@gmail.com>
2020-09-22 00:00:13 -0400

    color

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/yzwdroid/goURL
 go 1.15
 
 require (
+	github.com/fatih/color v1.9.0
+	github.com/gookit/color v1.3.1
+	github.com/logrusorgru/aurora v2.0.3+incompatible
+	github.com/logrusorgru/aurora/v3 v3.0.0 // indirect
 	github.com/spf13/pflag v1.0.5
 	mvdan.cc/xurls/v2 v2.2.0
 )
--- main.go
+++ main.go
@@ -1,24 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
+	"net/http"
+	"os"
 
+	"github.com/gookit/color"
 	"mvdan.cc/xurls/v2"
 )
 
+func removeDuplicate(urls []string) []string {
+	result := make([]string, 0, len(urls))
+	temp := map[string]struct{}{}
+	for _, item := range urls {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
+}
+
+func checkStatus(link string) {
+	resp, err := http.Head(link)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if resp.StatusCode == 200 {
+		color.Green.Println(link, "is alive")
+	}
+}
+
 func main() {
-	dat, err := ioutil.ReadFile("urls.txt")
+	if len(os.Args) == 1 {
+		fmt.Println(`usage called
+goURL filename
+goURL -v filename
+		`)
+		os.Exit(-1)
+	}
+
+	fmt.Println(os.Args[1:])
+	var dat []byte
+	for _, file := range os.Args[1:] {
+		d, err := ioutil.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+		dat = append(dat, d...)
+	}
 	// var ip *int = flag.Int("flagname", 1234, "help message for flagname")
 	// flag.Parse
-	if err != nil {
-		panic(err)
-	}
+
 	rxStrict := xurls.Strict()
 	// urls is a slice of strings
 	urls := rxStrict.FindAllString(string(dat), -1)
-	for _, u := range urls {
-		println(u)
-	}
 	println(len(urls))
-	//seen := make(map[string]bool)
+	urls = removeDuplicate(urls)
+	println(len(urls))
+	color.Green.Println("hello world")
+
+	for _, u := range urls {
+		checkStatus(u)
+	}
+	// var wg sync.WaitGroup
 }
